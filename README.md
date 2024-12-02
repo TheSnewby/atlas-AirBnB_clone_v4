b2d6dfe70304ccc9d878fa0596f534b59fcb44dd
Natdog225 <Nathaniel.wilson@atlasschool.com>
2024-12-02 13:42:02 -0600

    Got rid of the 4 for a few files as a test when running.

--- README.md
+++ README.md
@@ -157,9 +157,9 @@ No known bugs at this time.
 Alexa Orrico - [Github](https://github.com/alexaorrico) / [Twitter](https://twitter.com/alexa_orrico)  
 Jennifer Huang - [Github](https://github.com/jhuang10123) / [Twitter](https://twitter.com/earthtojhuang)  
 Jhoan Zamora - [Github](https://github.com/jzamora5) / [Twitter](https://twitter.com/JhoanZamora10)  
-David Ovalle - [Github](https://github.com/Nukemenonai) / [Twitter](https://twitter.com/disartDave)
-Stephen Newby - [Github](https://github.com/TheSnewby)
-Nathan Wilson - [Github] (https://github.com/Natdog225)
+David Ovalle - [Github](https://github.com/Nukemenonai) / [Twitter](https://twitter.com/disartDave)  
+Stephen Newby - [Github](https://github.com/TheSnewby)  
+Nathan Wilson - [Github] (https://github.com/Natdog225)  
 
 Second part of Airbnb: Joann Vuong
 ## License
---
+++ web_dynamic/static/scripts/hbnb.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+	// API status check
+	$.get('http://0.0.0.0:5001/api/v1/status/', (data, status) => {
+	  if (status === 'OK') {
+		$('div#api_status').addClass('available');
+	  } else {
+		$('div#api_status').removeClass('available');
+	  }
+	});
+  
+	// Fetch initial places
+	$.ajax({
+	  url: 'http://0.0.0.0:5001/api/v1/places_search',
+	  type: 'POST',
+	  contentType: 'application/json',
+	  data: JSON.stringify({}),
+	  success: function (response) {
+		for (const place of response) {
+		  const article = `
+			<article>
+			  <div class="title_box">
+				<h2>${place.name}</h2>
+				<div class="price_by_night">$${place.price_by_night}</div>
+			  </div>
+			  <div class="information">
+				<div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+				<div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+				<div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+			  </div>
+			  <div class="description">
+				${place.description} 
+			  </div>
+			</article>`;
+		  $('section.places').append(article);
+		}
+	  },
+	  error: function (error) {
+		console.error('Error fetching places:', error);
+	  }
+	});
+  
+	// Amenity filter logic
+	$('button').click(function () {
+	  const checkedAmenities = [];
+	  $('input[type="checkbox"]:checked').each(function () {
+		checkedAmenities.push($(this).data('id'));
+	  });
+  
+	  $.ajax({
+		url: 'http://0.0.0.0:5001/api/v1/places_search',
+		type: 'POST',
+		contentType: 'application/json',
+		data: JSON.stringify({ amenities: checkedAmenities }),
+		success: function (response) {
+		  $('section.places').empty(); // Clear previous places
+		  // Add new places
+		  for (const place of response) {
+			const article = `
+			  <article>
+				<div class="title_box">
+				  <h2>${place.name}</h2>
+				  <div class="price_by_night">$${place.price_by_night}</div>
+				</div>
+				<div class="information">
+				  <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+				  <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+				  <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+				</div>
+				<div class="description">
+				  ${place.description} 
+				</div>
+			  </article>`;
+			$('section.places').append(article);
+		  }
+		},
+		error: function (error) {
+		  console.error('Error fetching places:', error);
+		}
+	  });
+	});
+  });
